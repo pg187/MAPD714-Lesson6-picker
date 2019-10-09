225014b464ea7f3f6b548ecdb1fa2bda1a6e25a8
Pragya <prog187.pg@gmail.com>
2019-10-09 19:39:38 -0400

    Initial Commit

---
+++ MAPD714-Lesson6-picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-Lesson6-picker
+//
+//  Created by Student on 2019-10-09.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
